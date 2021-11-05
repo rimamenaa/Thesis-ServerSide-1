e680c497533f8b2c36f474e6f2e439fa6731fa87
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-11-05 09:49:57 +0100

    modification hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Deployment bycycle project ! yaani 7ootou lien deployment fel front whenever you need data from DB';
   }
 }
