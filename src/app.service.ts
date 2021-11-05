d0bc6853850609fd854d21dfe818d0fa2c600ff6
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-11-05 13:13:47 +0100

    review added

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Deployment bycycle project ! yaani 7ootou lien deployment fel front whenever you need data from DB';
+    return 'Deployment bycycle project ! yaani 7ootou lien deployment fel front whenever you need data from DB , Bycy-cle';
   }
 }
