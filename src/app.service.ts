10da7cbfa8447aa0152a589594db7f8eae8d1a9e
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-11-07 16:05:26 +0100

    added config to heroku

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return ' Deployment yemchi mriguel after Auth of wissem merged';
+    return ' .env added to config heroku ';
   }
 }
